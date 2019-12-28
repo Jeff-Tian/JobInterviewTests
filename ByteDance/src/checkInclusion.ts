112999c1e31c0c64bf052b422bc22040dce566be
Jeff Tian <jie.tian@unicareer.com>
2019-12-28 14:38:51 +0800

    feat: window

--- ByteDance/src/checkInclusion.ts
+++ ByteDance/src/checkInclusion.ts
@@ -1,7 +1,10 @@
 export const checkInclusion = (s1: string, s2: string) => {
-  const permutations = getPermutations(s1);
-  for (let p of Array.from(permutations)) {
-    if (s2.indexOf(p) >= 0) {
+  const windowLength = s1.length;
+  const amount = getAmount(s1);
+
+  for (let i = 0; i < s2.length - windowLength + 1; i++) {
+    const substr = s2.substr(i, windowLength);
+    if (equals(amount, getAmount(substr))) {
       return true;
     }
   }
@@ -9,63 +12,17 @@ export const checkInclusion = (s1: string, s2: string) => {
   return false;
 };
 
-export const getPermutations = (s: string): Set<string> => {
-  if (s.length === 1) {
-    return new Set([s]);
-  }
+export const getAmount = (s: string) =>
+  s.split('').reduce((previousValue: any, currentValue) => {
+    previousValue[currentValue] = previousValue[currentValue]
+      ? previousValue[currentValue] + 1
+      : 1;
+    return previousValue;
+  }, {});
 
-  const res = new Set<string>();
-
-  for (let i = 0; i < s.length; i++) {
-    let permutations = getPermutations(s.substr(0, i) + s.substr(i + 1));
-    permutations.forEach(p => res.add(s[i] + p));
-  }
-
-  return res;
-};
-
-export const factorial = (n: number): number => {
-  if (n === 1) {
-    return 1;
-  }
-
-  return n * factorial(n - 1);
-};
-
-let firstPart: string[] = [];
-const secondPart: string[] = [];
-
-export const getOrders = (s: string): any => {
-  for (let i = 0; i < s.length; i++) {
-    firstPart.push(s[i]);
-    secondPart.push(s.substr(0, i) + s.substr(i + 1));
-  }
-
-  const tmp = [];
-  for (let i = 0; i < secondPart.length; i++) {
-    const line = secondPart[i];
-
-    for (let j = 0; j < line.length; j++) {
-      tmp.push(firstPart[i] + line[j]);
-    }
-  }
-  firstPart = tmp;
-
-  for (let i = 0; i < firstPart.length; i++) {
-    firstPart[i] += subtract(s, firstPart[i]);
-  }
-
-  return firstPart;
-};
-
-export const subtract = (s1: string, s2: string) => {
-  let res = '';
-
-  for (let i = 0; i < s1.length; i++) {
-    if (s2.indexOf(s1[i]) < 0) {
-      res += s1[i];
-    }
-  }
-
-  return res;
+export const equals = (o1: any, o2: any) => {
+  return (
+    Object.keys(o1).length === Object.keys(o2).length &&
+    Object.keys(o1).filter(key => o1[key] !== o2[key]).length === 0
+  );
 };
--- ByteDance/test/字符串的排列.test.ts
+++ ByteDance/test/字符串的排列.test.ts
@@ -1,51 +1,37 @@
-import {
-  checkInclusion,
-  factorial,
-  getOrders,
-  getPermutations,
-  subtract,
-} from '../src/checkInclusion';
+import { checkInclusion, equals, getAmount } from '../src/checkInclusion';
 
 const testIt = (func?: Function) => (
-  expectedResult: boolean | string | string[] | Set<string> | number,
-  ...args: (string | number)[]
+  expectedResult:
+    | boolean
+    | string
+    | string[]
+    | Set<string>
+    | number
+    | { [key: string]: number },
+  ...args: (string | number | any)[]
 ) =>
-  it(args.join(' '), () => {
+  it(args.join(' ') + '--> ' + expectedResult, () => {
     let actual = (func || checkInclusion).apply(null, args);
     expect(actual).toEqual(expectedResult);
   });
 
-describe.skip('字符串的排列', () => {
+describe('字符串的排列', () => {
   testIt()(true, 'ab', 'eidbaooo');
   testIt()(false, 'ab', 'eidboaoo');
   testIt()(true, 'ab', 'ab');
-  testIt()(false, '', '');
+  testIt()(true, 'a', 'a');
+  testIt()(true, '', '');
   testIt()(false, 'ab', '');
   testIt()(true, 'adc', 'dcda');
   testIt()(true, 'abc', 'cccccbabbbaaaa');
-  testIt()(true, 'dinitrophenylhydrazine', 'acetylphenylhydrazine');
+  testIt()(false, 'dinitrophenylhydrazine', 'acetylphenylhydrazine');
 });
 
-describe('permutations', () => {
-  testIt(getPermutations)(new Set(['ba', 'ab']), 'ba');
-  testIt(getPermutations)(new Set([]), '');
-  testIt(getPermutations)(new Set(['a']), 'a');
-  testIt(getPermutations)(
-    new Set(['abc', 'bac', 'cba', 'acb', 'bca', 'cab']),
-    'abc'
-  );
+describe('get Amount', () => {
+  testIt(getAmount)({ a: 2, b: 1 }, 'aab');
 });
 
-describe('factorial', () => {
-  testIt(factorial)(1, 1);
-  testIt(factorial)(2, 2);
-  testIt(factorial)(6, 3);
-});
-
-describe('getOrders', () => {
-  testIt(getOrders)(['123', '132', '213', '231', '312', '321'], '123');
-});
-
-describe('subtract', () => {
-  testIt(subtract)('1', '123', '23');
+describe('equals', () => {
+  testIt(equals)(true, { a: 2, b: 1 }, { a: 2, b: 1 });
+  testIt(equals)(false, { a: 2, b: 1 }, { a: 2, b: 1, c: 1 });
 });
