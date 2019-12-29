357aef280fa96c25da793c7cebb4db5306781079
Jeff Tian <jeff.tian@outlook.com>
2019-12-29 14:12:56 +0800

    refactor: curry

--- ByteDance/src/longestCommonPrefix.ts
+++ ByteDance/src/longestCommonPrefix.ts
@@ -1,11 +1,17 @@
-export var longestCommonPrefix = function(strs: string[]) {
+const curry = (fn: Function) => (leftMostArg: any) => (...args: any[]) => fn(leftMostArg, ...args)
+
+const startsWith = curry((prefix: string, s: string) => s.startsWith(prefix))
+
+export var longestCommonPrefix = function (strs: string[]) {
   if (strs.length <= 0) {
     return '';
   }
 
   for (let i = 0; i < strs[0].length; i++) {
+    const prefix = strs[0].substr(0, i + 1)
+
     if (
-      strs.filter(s => s.indexOf(strs[0].substr(0, i + 1)) === 0).length !==
+      strs.filter(startsWith(prefix)).length !==
       strs.length
     ) {
       return strs[0].substr(0, i);
