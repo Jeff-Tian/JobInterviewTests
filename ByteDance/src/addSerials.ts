5f5cbca138554cbcaa57ac357c5651ce73791036
Jeff Tian <jeff.tian@outlook.com>
2020-01-07 20:18:31 +0800

    refactor: inline

--- ByteDance/src/addSerials.ts
+++ ByteDance/src/addSerials.ts
@@ -1,22 +1,18 @@
 export const addSerials = (a: string[]) => {
-    const stats = a.reduce((prev: any, next) => {
-        prev[next] = -~prev[next];
+  const stats = a.reduce((prev: any, next) => {
+    prev[next] = -~prev[next];
 
-        return prev;
-    }, {})
+    return prev;
+  }, {});
 
-    Object.keys(stats).forEach(key => {
-        if (stats[key] === 1) {
-            stats[key] = ''
-        }
-    })
-
-    let res = [];
-
-    for (let i = a.length - 1; i >= 0; i--) {
-        res.unshift(a[i] + stats[a[i]])
-        stats[a[i]]--;
+  Object.keys(stats).forEach(key => {
+    if (stats[key] === 1) {
+      stats[key] = false;
     }
+  });
 
-    return res;
-}
+  return a
+    .reverse()
+    .map(item => item + (stats[item] ? stats[item]-- : ''))
+    .reverse();
+};
--- ByteDance/test/编号.test.ts
+++ ByteDance/test/编号.test.ts
@@ -1,8 +1,8 @@
-import { addSerials } from "../src/addSerials"
+import { addSerials } from '../src/addSerials';
 
 describe.only('编号', () => {
-    it('加序号', () => {
-        const res = addSerials(['ab', 'c', 'ab', 'd', 'c'])
-        expect(res).toEqual(['ab1', 'c1', 'ab2', 'd', 'c2'])
-    })
-})
+  it('加序号', () => {
+    const res = addSerials(['ab', 'c', 'ab', 'd', 'c']);
+    expect(res).toEqual(['ab1', 'c1', 'ab2', 'd', 'c2']);
+  });
+});
