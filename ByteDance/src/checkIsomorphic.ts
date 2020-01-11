01fc9bb7ce84ff070fd72caec8dbb59add69f7ef
Jeff Tian <jie.tian@unicareer.com>
2020-01-11 15:24:55 +0800

    refactor: by compose

--- ByteDance/src/checkIsomorphic.ts
+++ ByteDance/src/checkIsomorphic.ts
@@ -1,24 +1,23 @@
-export const checkIsomorphic = (s1: string, s2: string) => {
-  if (s1.length !== s2.length) {
-    return false;
-  }
+type Mapper = (i: any) => any
 
-  const serials1 = [];
-  const serials2 = [];
+const map = (func: Mapper) => (target: any[]) => target.map(func)
+const compose = (f: Function, g: Function) => f(g)
+const higherOrderCompose = (f: Function, g: Function) => (...x: any) => f(g(...x))
+const subtract = ([a, b]: number[]) => a - b
+const subtractBy = (func: Mapper) => higherOrderCompose(subtract, compose(map, func))
+const charCodeAt = (s: string, i: number) => s.charCodeAt(i)
+const charCodeAtFirstLetter = (s: string) => charCodeAt(s, 0)
+const higherOrderSubtractBy = (s: string) => (i: string) => subtractBy(charCodeAtFirstLetter)([i, s])
+const stringToArray = (str: string) => [...str]
+const mapString = (str: string, mapper: any) => stringToArray(str).map(mapper)
+const join = (a: any[], sep: string) => a.join(sep)
+const joinDirectly = (a: any[]) => join(a, '')
+const structure = (s: string) => joinDirectly(mapString(s, higherOrderSubtractBy(s)))
 
-  for (let i = 0; i < s1.length; i++) {
-    serials1.push(s1.charCodeAt(i) - s1.charCodeAt(0));
-  }
+const equal = ([a, b]: string[]) => a === b
+const equalBy = (func: Mapper) => (a: string, b: string) => {
+    return equal(compose(map, func)([a, b]))
+}
 
-  for (let i = 0; i < s2.length; i++) {
-    serials2.push(s2.charCodeAt(i) - s2.charCodeAt(0));
-  }
 
-  for (let i = 0; i < serials1.length; i++) {
-    if (serials1[i] !== serials2[i]) {
-      return false;
-    }
-  }
-
-  return true;
-};
+export const checkIsomorphic = compose(equalBy, structure)
