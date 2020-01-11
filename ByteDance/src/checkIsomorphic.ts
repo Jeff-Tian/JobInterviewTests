7b574d6ec89f26a196ec2848c95107051297af44
Jeff Tian <jie.tian@unicareer.com>
2020-01-11 16:04:37 +0800

    refactor: by by

--- ByteDance/src/checkIsomorphic.ts
+++ ByteDance/src/checkIsomorphic.ts
@@ -1,23 +1,22 @@
-type Mapper = (i: any) => any
+type Mapper = (i: any) => any;
 
-const map = (func: Mapper) => (target: any[]) => target.map(func)
-const compose = (f: Function, g: Function) => f(g)
-const higherOrderCompose = (f: Function, g: Function) => (...x: any) => f(g(...x))
+const map = (func: Mapper) => (...target: any[]) => target.map(func)
+const compose = (f: Function, g: Function) => (...x: any) =>
+    f(g(...x))
+
+const by = (what: Function) => (mapper: Mapper) => compose(what, map(mapper))
+
 const subtract = ([a, b]: number[]) => a - b
-const subtractBy = (func: Mapper) => higherOrderCompose(subtract, compose(map, func))
-const charCodeAt = (s: string, i: number) => s.charCodeAt(i)
-const charCodeAtFirstLetter = (s: string) => charCodeAt(s, 0)
-const higherOrderSubtractBy = (s: string) => (i: string) => subtractBy(charCodeAtFirstLetter)([i, s])
-const stringToArray = (str: string) => [...str]
-const mapString = (str: string, mapper: any) => stringToArray(str).map(mapper)
+const equal = ([a, b]: string[]) => a === b
+
+const charCodeAt = (i: number) => (s: string) => s.charCodeAt(i)
+const charCodeAtFirstLetter = charCodeAt(0)
+const higherOrderSubtractBy = (s: string) => (i: string) =>
+    by(subtract)(charCodeAtFirstLetter)(i, s)
+const mapString = (str: string) => (mapper: any) => map(mapper)(...str)
 const join = (a: any[], sep: string) => a.join(sep)
 const joinDirectly = (a: any[]) => join(a, '')
-const structure = (s: string) => joinDirectly(mapString(s, higherOrderSubtractBy(s)))
+const structure = (s: string) => compose(joinDirectly, mapString(s))(higherOrderSubtractBy(s))
 
-const equal = ([a, b]: string[]) => a === b
-const equalBy = (func: Mapper) => (a: string, b: string) => {
-    return equal(compose(map, func)([a, b]))
-}
-
-
-export const checkIsomorphic = compose(equalBy, structure)
+export const checkIsomorphic =
+    by(equal)(structure)
