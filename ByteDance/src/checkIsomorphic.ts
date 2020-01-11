ec3ac079bf69b9e5bae3a5765d470a0b3d7eac4d
Jeff Tian <jie.tian@unicareer.com>
2020-01-11 16:04:47 +0800

    refactor: by by

--- ByteDance/src/checkIsomorphic.ts
+++ ByteDance/src/checkIsomorphic.ts
@@ -1,22 +1,21 @@
 type Mapper = (i: any) => any;
 
-const map = (func: Mapper) => (...target: any[]) => target.map(func)
-const compose = (f: Function, g: Function) => (...x: any) =>
-    f(g(...x))
+const map = (func: Mapper) => (...target: any[]) => target.map(func);
+const compose = (f: Function, g: Function) => (...x: any) => f(g(...x));
 
-const by = (what: Function) => (mapper: Mapper) => compose(what, map(mapper))
+const by = (what: Function) => (mapper: Mapper) => compose(what, map(mapper));
 
-const subtract = ([a, b]: number[]) => a - b
-const equal = ([a, b]: string[]) => a === b
+const subtract = ([a, b]: number[]) => a - b;
+const equal = ([a, b]: string[]) => a === b;
 
-const charCodeAt = (i: number) => (s: string) => s.charCodeAt(i)
-const charCodeAtFirstLetter = charCodeAt(0)
+const charCodeAt = (i: number) => (s: string) => s.charCodeAt(i);
+const charCodeAtFirstLetter = charCodeAt(0);
 const higherOrderSubtractBy = (s: string) => (i: string) =>
-    by(subtract)(charCodeAtFirstLetter)(i, s)
-const mapString = (str: string) => (mapper: any) => map(mapper)(...str)
-const join = (a: any[], sep: string) => a.join(sep)
-const joinDirectly = (a: any[]) => join(a, '')
-const structure = (s: string) => compose(joinDirectly, mapString(s))(higherOrderSubtractBy(s))
+  by(subtract)(charCodeAtFirstLetter)(i, s);
+const mapString = (str: string) => (mapper: any) => map(mapper)(...str);
+const join = (a: any[], sep: string) => a.join(sep);
+const joinDirectly = (a: any[]) => join(a, '');
+const structure = (s: string) =>
+  compose(joinDirectly, mapString(s))(higherOrderSubtractBy(s));
 
-export const checkIsomorphic =
-    by(equal)(structure)
+export const checkIsomorphic = by(equal)(structure);
