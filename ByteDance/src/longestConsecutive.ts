3959fc47d39997745e09c35d2bd0ce33e3875e3a
Jeff Tian <jie.tian@unicareer.com>
2020-01-14 14:35:40 +0800

    feat: 最长连续序列

--- ByteDance/src/longestConsecutive.ts
+++ ByteDance/src/longestConsecutive.ts
@@ -1,1 +1,26 @@
-export const longestConsecutive = (nums: number[]) => {};
+export const longestConsecutive = (nums: number[]) => {
+  let max = 0;
+  let current = 0;
+
+  if (nums.length === 0) {
+    return 0;
+  }
+
+  nums
+    .sort((x, y) => x - y)
+    .slice(1)
+    .reduce((prev, next) => {
+      if (next - prev === 1) {
+        current++;
+      } else if (next - prev > 1) {
+        current = 0;
+      }
+
+      if (current > max) {
+        max = current;
+      }
+      return next;
+    }, nums[0]);
+
+  return max + 1;
+};
--- ByteDance/test/最长连续序列.test.ts
+++ ByteDance/test/最长连续序列.test.ts
@@ -1,5 +1,9 @@
 import { testIt } from './helper';
+import { longestConsecutive } from '../src/longestConsecutive';
 
 describe('最长连续序列', () => {
   testIt(longestConsecutive)(4, [100, 4, 200, 1, 3, 2]);
+  testIt(longestConsecutive)(0, []);
+  testIt(longestConsecutive)(3, [1, 2, 0, 1]);
+  testIt(longestConsecutive)(1, [0, 0]);
 });
