b4a2db547abb9ba82ed8099772e0c8c467799a45
Jeff Tian <jie.tian@unicareer.com>
2020-01-15 17:43:53 +0800

    feat: get permutations

---
+++ ByteDance/src/getPermutation.ts
@@ -0,0 +1,63 @@
+export const memoize = (fn: Function) => (...args: any) => {
+    const cache: any = {}
+    const key = JSON.stringify(args)
+    if (!cache[key]) {
+        cache[key] = fn(...args)
+    }
+
+    return cache[key]
+}
+
+export const factorial = memoize((n: number): number => {
+    if (n <= 0) {
+        return 0
+    }
+
+    if (n === 1) {
+        return 1
+    }
+
+    return n * factorial(n - 1)
+})
+
+export const getPermutations = (base: string, k: number = factorial(base.length)) => {
+    if (base.length <= 0) {
+        return []
+    }
+
+    if (base.length === 1) {
+        return ['1']
+    }
+
+    if (base.length === 2) {
+        return [[base[0], base[1]].join(''), [base[1], base[0]].join('')]
+    }
+
+    let res: string[] = []
+    for (let i = 0; i < base.length; i++) {
+        const sub = base.substr(0, i) + base.substr(i + 1)
+
+        if (res.length >= k) {
+            return res
+        }
+
+        res = [...res, ...getPermutations(sub, k).map((e: string) => base[i] + e)]
+
+        if (res.length >= k) {
+            return res
+        }
+    }
+
+    return res
+}
+
+export const getPermutationsOf = (base: string, k: number) => getPermutations(base)[k - 1]
+
+export const getPermutation = (n: number, k: number) => {
+    if (n === 1 && k === 1) {
+        return '1'
+    }
+
+    const base = new Array(n).fill(0)
+    return getPermutationsOf(base.map((_, index) => (index + 1)).join(''), k)
+}
---
+++ ByteDance/test/第k个排列.test.ts
@@ -0,0 +1,28 @@
+import {testIt} from './helper'
+import {factorial, getPermutation, getPermutations, getPermutationsOf} from '../src/getPermutation'
+
+describe('阶乘', () => {
+    testIt(factorial)(1, 1)
+    testIt(factorial)(2, 2)
+    testIt(factorial)(6, 3)
+})
+
+describe('第k个排列', () => {
+    testIt(getPermutation)('1', 1, 1)
+    testIt(getPermutation)('12', 2, 1)
+    testIt(getPermutation)('21', 2, 2)
+    testIt(getPermutation)('123', 3, 1)
+    testIt(getPermutation)('132', 3, 2)
+})
+
+describe('全排列', () => {
+    testIt(getPermutations)(['12', '21'], '12')
+    testIt(getPermutationsOf)('123', '123', 1)
+    testIt(getPermutationsOf)('132', '123', 2)
+    testIt(getPermutations)(["123",
+        "132",
+        "213",
+        "231",
+        "312",
+        "321"], '123')
+})
