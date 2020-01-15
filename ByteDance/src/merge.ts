d3201ad1c8f10b2c5107fc6ec8f869f3a7cced95
Jeff Tian <jie.tian@unicareer.com>
2020-01-15 18:12:22 +0800

    feat: 合并区间

---
+++ ByteDance/src/merge.ts
@@ -0,0 +1,18 @@
+export const merge = (intervals: number[][]) => {
+    return intervals
+        .sort((prev, next) => prev[0] - next[0]).reduce((res: number[][], next: number[]
+            ) => {
+                const prev = res[res.length - 1]
+
+                if (prev && prev[1] >= next[0]) {
+                    prev[1] = Math.max(prev[1], next[1])
+                } else {
+                    res.push(next)
+                }
+
+                return res
+            }
+            ,
+            []
+        )
+}
---
+++ ByteDance/test/合并区间.test.ts
@@ -0,0 +1,8 @@
+import {testIt} from "./helper"
+import {merge} from "../src/merge"
+
+describe('合并区间', () => {
+    testIt(merge)([[1, 6], [8, 10], [15, 18]], [[1, 3], [2, 6], [8, 10], [15, 18]])
+    testIt(merge)([[1, 5]], [[1, 4], [4, 5]])
+    testIt(merge)([[0, 4]], [[1, 4], [0, 4]])
+})
